cd3076a8e0fc61f3fc2e249a397c03db3df22d55
Jesse Claven <jesse.claven@me.com>
2024-09-07 20:27:29 +0100

    refactor(data): Clean-up embedding phase

--- data/01-from-notes-app-to-plaintext/steps.md
+++ data/01-from-notes-app-to-plaintext/steps.md
@@ -1,3 +1,5 @@
+# Get the data
+
 cd ~/src/github.com/threeplanetssoftware/apple_cloud_notes_parser/
 mkdir -p data
 cp ~/Library/Group\ Containers/group.com.apple.notes/NoteStore.sqlite data/NoteStore.sqlite
@@ -5,4 +7,6 @@ bundle install
 ruby notes_cloud_ripper.rb -f data/NoteStore.sqlite --individual-files -o output
 mv output/{TIMESTAMP} ~/src/github.com/jesse-c/notes-app-hybrid-search/data/01-from-notes-app-to-plaintext
 cd ~/src/github.com/jesse-c/notes-app-hybrid-search
-head -n 5 data/01-from-notes-app-to-plaintext/output/{TIMESTAMP}/csv/note_store_notes_1.csv
+xsv sample 5 data/01-from-notes-app-to-plaintext/output/{TIMESTAMP}/csv/note_store_notes_1.csv
+
+# Transform the data
--- data/02-plaintext-to-embeddings/main.py
+++ data/02-plaintext-to-embeddings/main.py
@@ -1,15 +1,50 @@
-from sentence_transformers import SentenceTransformer
 import numpy as np
+import tomllib
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
 
-sentences = [
-    # "https://www.ruinedby.design\nRUINED BY DESIGN\nA design ethics and activism book by Mike Monteiro"
-    "art"
-]
+import structlog
 
-model = SentenceTransformer("sentence-transformers/msmarco-MiniLM-L-6-v3")
-embeddings = model.encode(sentences)
+log = structlog.get_logger()
+
+# Load configuration
+log.debug("config", status="loading")
+with open("config.toml", "rb") as f:
+    config = tomllib.load(f)
+log.debug("config", status="loaded")
+
+# Read sentences from input file
+log.debug("sentences", status="loading")
+with open(config["data"]["input_file"], "r") as f:
+    sentences = f.readlines()
+log.debug("sentences", status="loaded")
+
+# Remove any leading/trailing whitespace
+log.debug("sentences", status="stripping")
+sentences = [s.strip() for s in sentences]
+log.debug("sentences", status="stripped")
+
+log.debug("model", status="loading")
+model = SentenceTransformer(config["model"]["name"])
+log.debug("model", status="loaded")
+
+log.debug("embeddings", status="generating")
+embeddings = []
+
+for i in tqdm(range(0, len(sentences), config["embedding"]["batch_size"])):
+    batch = sentences[i : i + config["embedding"]["batch_size"]]
+    batch_embeddings = model.encode(batch)
+    embeddings.extend(np.array(batch_embeddings))
+
+log.debug("embeddings", status="generated")
+
 
 # Save embeddings to a file without scientific notation
-np.savetxt("embeddings.txt", embeddings, delimiter=",", fmt="%.8f")
-
-print("Embeddings have been saved to 'embeddings.txt'")
+log.debug("embeddings", status="saving")
+np.savetxt(
+    config["data"]["output_file"],
+    embeddings,
+    delimiter=config["embedding"]["delimiter"],
+    fmt=config["embedding"]["float_format"],
+)
+log.debug("embeddings", status="save")
